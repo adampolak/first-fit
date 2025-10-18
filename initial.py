f7b083a3d3fc948ed0fa998ab7c92612867d8b9d
Adam Polak <adam.polak@unibocconi.it>
2025-10-18 22:54:18 +0200

    Better initial solution

--- initial.py
+++ initial.py
@@ -8,12 +8,39 @@ def construct_intervals():
   divided by the maximum number of intervals that cover a single point
   
   The initial implementation uses the construction from
-  Figure 2 in https://arxiv.org/abs/1506.00192
+  Figure 4 in https://arxiv.org/abs/1506.00192
   
   Returns:
     intervals: list of tuples, each tuple (l, r) represents an open interval from l to r
   """
-  return [(1, 4), (7, 10), (3, 6), (5, 8)]
+
+  T = [(0, 1)]
+  for _ in range(2):
+    lo = min(l for l, r in T)
+    hi = max(r for l, r in T)
+    delta = hi - lo
+    S = []
+    for start in (2, 6, 10, 14):
+      S += [(delta * start + l - lo, delta * start + r - lo) for l, r in T]
+    S += [
+      (delta * 1, delta * 5),
+      (delta * 12, delta * 16),
+      (delta * 4, delta * 9),
+      (delta * 8, delta * 13)
+    ]
+    T = S
+  return T
+
+  # return [  # Figure 3, OPT=2, FF=4
+  #   (2,3),
+  #   (6,7),
+  #   (10,11),
+  #   (14,15),
+  #   (1,5),
+  #   (12,16),
+  #   (4,9),
+  #   (8,13),
+  # ]
 
 # EVOLVE-BLOCK-END
 
