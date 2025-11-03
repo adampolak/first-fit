108b2ef61e4f94b3ed260e3b909567fd506affad
Adam Polak <adam.polak@unibocconi.it>
2025-11-03 15:51:15 +0100

    Initial solution contains code for two other (worse) solutions

--- initial.py
+++ initial.py
@@ -1,19 +1,24 @@
 # EVOLVE-BLOCK-START
 
-def construct_intervals():
-  """
-  Construct a sequence of intervals of real line,
-  in the order in which they are presented to FirstFit,
-  so that it maximizes the number of colors used by FirstFit
-  divided by the maximum number of intervals that cover a single point
-  
-  The initial implementation uses the construction from
-  Figure 4 in https://arxiv.org/abs/1506.00192
-  
-  Returns:
-    intervals: list of tuples, each tuple (l, r) represents an open interval from l to r
-  """
+def construct3vs2:
+  # The construction from Figure 2 in https://arxiv.org/abs/1506.00192
+  return [(1, 4), (7, 10), (3, 6), (5, 8)]
 
+def construct4vs2():
+  # The construction from Figure 3 in https://arxiv.org/abs/1506.00192
+  return [
+    (2,3),
+    (6,7),
+    (10,11),
+    (14,15),
+    (1,5),
+    (12,16),
+    (4,9),
+    (8,13),
+  ]
+
+def construct7vs4():
+  # The construction from Figure 4 in https://arxiv.org/abs/1506.00192
   T = [(0, 1)]
   for _ in range(2):
     lo = min(l for l, r in T)
@@ -31,17 +36,21 @@ def construct_intervals():
     T = S
   return T
 
-  # return [  # Figure 3, OPT=2, FF=4
-  #   (2,3),
-  #   (6,7),
-  #   (10,11),
-  #   (14,15),
-  #   (1,5),
-  #   (12,16),
-  #   (4,9),
-  #   (8,13),
-  # ]
-
+def construct_intervals():
+  """
+  Construct a sequence of intervals of real line,
+  in the order in which they are presented to FirstFit,
+  so that it maximizes the number of colors used by FirstFit
+  divided by the maximum number of intervals that cover a single point
+  
+  The initial implementation uses the construction from
+  Figure 4 in https://arxiv.org/abs/1506.00192
+  
+  Returns:
+    intervals: list of tuples, each tuple (l, r) represents an open interval from l to r
+  """
+  return construct7vs4()
+  
 # EVOLVE-BLOCK-END
 
 def run_experiment(**kwargs):
